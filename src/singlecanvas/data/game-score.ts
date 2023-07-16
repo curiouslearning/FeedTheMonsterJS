a85a0e010a6c9c16c3948a719f26a791b988890b
Rakshith <62013445+rakshith-arch@users.noreply.github.com>
2023-07-16 19:04:06 +0530

    added the updation of monster phases

--- src/singlecanvas/data/game-score.ts
+++ src/singlecanvas/data/game-score.ts
@@ -4,12 +4,14 @@ export class GameScore {
   public static currentlanguage: string = lang;
 
   public static setGameLevelScore(currentLevelInfo, score) {
+    let starsGained = this.calculateStarCount(score);
     let levelPlayedInfo = {
       levelName: currentLevelInfo.levelMeta.levelType,
       levelNumber: currentLevelInfo.levelMeta.levelNumber,
       score: score,
-      starCount: this.getStarCount(score),
+      starCount: starsGained,
     };
+    this.setTotalStarCount(starsGained);
     let allGamelevelInfo: any[] = this.getAllGameLevelInfo();
     allGamelevelInfo.push(levelPlayedInfo);
     localStorage.setItem("gamePlayedInfo", JSON.stringify(allGamelevelInfo));
@@ -19,8 +21,20 @@ export class GameScore {
     const data = localStorage.getItem(this.currentlanguage + "gamePlayedInfo");
     return data == undefined ? [] : JSON.parse(data);
   }
+
+  public static setTotalStarCount(starsGained){
+    let starCount = this.getTotalStarCount();
+    let totalStarCount = starCount + starsGained;
+    localStorage.setItem(this.currentlanguage + "totalStarCount",totalStarCount);
+
+  }
+
+  public static getTotalStarCount(){
+    const starCount = localStorage.getItem(this.currentlanguage + "totalStarCount");
+    return starCount == undefined ? 0: parseInt(starCount);
+  }
   
-  public static getStarCount(score: number): number {
+  public static calculateStarCount(score: number): number {
     return score == 200
       ? 1
       : score == 300
