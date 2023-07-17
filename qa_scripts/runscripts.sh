dba9fc010a0a36f32b3e2c3278d3f36da770b55f
Vinay <62736791+vinay961861@users.noreply.github.com>
2023-07-17 12:11:00 +0530

    Fixes unicode issue

---
+++ qa_scripts/runscripts.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+owner="curiouslearning"
+repo="ftm-languagepacks"
+token="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"
+response=$(curl -s -H "Authorization: token $token" "https://api.github.com/repos/$owner/$repo/contents")
+folders=($(echo "$response" | jq -r '.[] | select(.type == "dir") | .name'))
+
+echo "Folders in the GitHub repository '$owner/$repo':"
+
+PS3="Select a folder number: "
+select folder in "${folders[@]}"; do
+    if [[ -n $folder ]]; then
+        echo "You selected: $folder"
+        selected_lang="$folder" 
+        break
+    else
+        echo "Invalid selection. Please try again."
+    fi
+done
+echo "Executing------------->firstScript"
+
+node find_missing_foilstone_paths.js "$folder"  # First Script (Javascript)
+
+echo "executed------------->firstScript"
+
+echo "Executing------------->secondScript"
+
+echo "Downloading Audios,Please wait........."
+
+secd_scrpt_output=$(python3 download_audio_files.py "$folder")  #Second Script (Python)
+
+echo "executed------------->secondScript"
+# Use the desired values in subsequent operations
+echo "Output from second script:$secd_scrpt_output"
+
+# Extract the desired value from the output
