cae04235517cba317e28214145a4beb8c63702cf
Vinay <62736791+vinay961861@users.noreply.github.com>
2023-07-19 11:55:01 +0530

    Optimising the scripts

--- qa_scripts/runscripts.sh
+++ qa_scripts/runscripts.sh
@@ -2,14 +2,15 @@
 
 owner="curiouslearning"
 repo="ftm-languagepacks"
+options=("Yes" "No")
+build=true
 # provide your access token 
-token=""
+token="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"
 response=$(curl -s -H "Authorization: token $token" "https://api.github.com/repos/$owner/$repo/contents")
 folders=($(echo "$response" | jq -r '.[] | select(.type == "dir") | .name'))
-
 echo "Folders in the GitHub repository '$owner/$repo':"
-
-PS3="Select a folder number: "
+#echo "response$response"
+PS3="Select a language number: "
 select folder in "${folders[@]}"; do
     if [[ -n $folder ]]; then
         echo "You selected: $folder"
@@ -19,20 +20,27 @@ select folder in "${folders[@]}"; do
         echo "Invalid selection. Please try again."
     fi
 done
-echo "Executing------------->firstScript"
-
+echo "Generating report for $folder language Please wait..... "
+python3 create_folder.py "$folder"
 node find_missing_foilstone_paths.js "$folder"  # First Script (Javascript)
-
-echo "executed------------->firstScript"
-
-echo "Executing------------->secondScript"
-
-echo "Downloading Audios,Please wait........."
-
-secd_scrpt_output=$(python3 download_audio_files.py "$folder")  #Second Script (Python)
-
-echo "executed------------->secondScript"
-# Use the desired values in subsequent operations
-echo "Output from second script:$secd_scrpt_output"
-
-# Extract the desired value from the output
+python3 download_audio_files.py "$folder"
+echo "Generated report for $folder language and writen to language_report.txt "
+echo "Do you want to build language"
+select option in "${options[@]}"; do
+  case $option in
+    "Yes")
+      echo "You selected yes."
+      node find_missing_foilstone_paths.js "$folder" "$build" 
+      echo "Done"
+      python3 download_audio_files.py "$folder" "$build"
+      break
+      ;;
+    "No")
+       echo "Exiting..."
+      break
+      ;;
+    *)
+      echo "Invalid option. Please select a valid number."
+      ;;
+  esac
+done
