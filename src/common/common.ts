0ae4ed3fae740d13e86db7e9d392aca07c8514a6
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2023-08-24 09:32:31 +0530

    Fixed #604

--- src/common/common.ts
+++ src/common/common.ts
@@ -21,8 +21,6 @@ export function loadImages(sources: any, callback: any) {
 }
 export function loadingScreen(loading: boolean) {
   const loadingElement = <HTMLElement>document.getElementById("loading-screen");
-  const loadingText = <HTMLElement>document.getElementById("loading_text");
-  loadingText.style.display = "none";
   if (loading) {
     loadingElement.style.display = "block";
     new CanvasStack("canvas").bkgCanvas.layers.forEach((element) => {
@@ -31,6 +29,7 @@ export function loadingScreen(loading: boolean) {
     });
   } else {
     loadingElement.style.display = "none";
+    progressContainer.style.display="none";
     new CanvasStack("canvas").bkgCanvas.layers.forEach((element) => {
       const htmlElement = <HTMLElement>document.getElementById(element.id);
       htmlElement.style.display = "flex";
