99c750548cb726ea01396a470812f9f59301906b
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2023-10-24 11:44:03 +0530

    Revert "Merge branch 'nikhil77' of https://github.com/curiouslearning/FeedTheMonsterJS into nikhil77"
    This reverts commit a30d9f959ea491def766f798991e5fa8f5af8a05, reversing
    changes made to bf5a28000702a873816e3620d3aa2f234f234f0e.

--- src/singlecanvas/data/game-score.ts
+++ src/singlecanvas/data/game-score.ts
@@ -1,4 +1,4 @@
-import { Debugger, lang } from "../../../global-variables";
+import { lang } from "../../../global-variables";
 
 export class GameScore {
   public static currentlanguage: string = lang;
@@ -25,24 +25,24 @@ export class GameScore {
     } else {
       allGamelevelInfo.push(levelPlayedInfo);
     }
-   Debugger.DebugMode?localStorage.setItem(this.currentlanguage +"Debug"+ "gamePlayedInfo", JSON.stringify(allGamelevelInfo)):localStorage.setItem(this.currentlanguage + "gamePlayedInfo", JSON.stringify(allGamelevelInfo));
+    localStorage.setItem(this.currentlanguage + "gamePlayedInfo", JSON.stringify(allGamelevelInfo));
   }
 
 
   public static getAllGameLevelInfo(): Map<string, any>[] {
-    const data = Debugger.DebugMode?localStorage.getItem(this.currentlanguage +"Debug"+ "gamePlayedInfo"):localStorage.getItem(this.currentlanguage + "gamePlayedInfo");
+    const data = localStorage.getItem(this.currentlanguage + "gamePlayedInfo");
     return data == undefined ? [] : JSON.parse(data) as Map<string, any>[];
   }
 
   public static setTotalStarCount(starsGained){
     let starCount = this.getTotalStarCount();
     let totalStarCount = starCount + starsGained;
-    Debugger.DebugMode?localStorage.setItem(this.currentlanguage +"Debug"+ "totalStarCount",totalStarCount):localStorage.setItem(this.currentlanguage + "totalStarCount",totalStarCount);
+    localStorage.setItem(this.currentlanguage + "totalStarCount",totalStarCount);
 
   }
 
   public static getTotalStarCount(){
-    const starCount =Debugger.DebugMode?localStorage.getItem(this.currentlanguage +"Debug"+ "totalStarCount"):localStorage.getItem(this.currentlanguage + "totalStarCount");
+    const starCount = localStorage.getItem(this.currentlanguage + "totalStarCount");
     return starCount == undefined ? 0: parseInt(starCount);
   }
   
