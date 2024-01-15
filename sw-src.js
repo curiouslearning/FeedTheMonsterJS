912e72399b3325003e3af7fcf884cb929b0cf35f
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2024-01-15 12:34:50 +0530

    Updated Tsonga Build Folder

--- sw-src.js
+++ sw-src.js
@@ -6,7 +6,7 @@ workbox.precaching.precacheAndRoute(self.__WB_MANIFEST, {
   exclude: [/^lang\//],
 });
 var number = 0;
-var version = 1.13;
+var version = 1.14;
 // self.addEventListener('activate', function(e) {
 //     console.log("activated");
 //
