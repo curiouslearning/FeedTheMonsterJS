d51f9db2f239288ea08e9b967a73753ec728d8d4
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2024-01-19 12:24:13 +0530

    Addeed Veitnamese Audios!

--- sw-src.js
+++ sw-src.js
@@ -6,7 +6,7 @@ workbox.precaching.precacheAndRoute(self.__WB_MANIFEST, {
   exclude: [/^lang\//],
 });
 var number = 0;
-var version = 1.14;
+var version = 1.15;
 // self.addEventListener('activate', function(e) {
 //     console.log("activated");
 //
