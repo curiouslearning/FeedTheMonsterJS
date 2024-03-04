b3628df03ac3473f628b865c752aa1da14d4e2f3
Nikhil77sutara <nikhilchoudhary@sutara.org>
2024-03-04 12:00:46 +0530

    ..

--- src/Firebase/base-firebase-integration.ts
+++ src/Firebase/base-firebase-integration.ts
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import {
   getAnalytics,
   logEvent,
+  setUserId,
   setUserProperties,
 } from "firebase/analytics";
 import { firebaseConfig } from "./firebase-config";
@@ -10,21 +11,33 @@ import { lang, pseudoId } from "../../global-variables";
 export class BaseFirebaseIntegration {
   firebaseApp: any;
   analytics: any;
+  userProperties: any = {};
   constructor() {
     this.initializeFirebase()
+    // this.setInitialUserProperties();
   }
-
+  // protected setInitialUserProperties(): void {
+  //   // Set initial user properties at the beginning of the session
+  //   setUserId(this.analytics,"55555");
+  //   this.userProperties.cr_user_id = pseudoId;
+  //   this.userProperties.ftm_language =lang;
+  //   this.userProperties.profile_number =0;
+  //   this.userProperties.version_number =  document.getElementById("version-info-id").innerHTML;
+  //   this.userProperties.json_version_number =  new Date().getTime().toString();
+  //   setUserProperties(this.analytics,this.userProperties);
+  // }
   protected customEvents(eventName: string, event: object): void {
     try {
-      this.initializeFirebase()
-      setUserProperties(this.analytics, {
-        cr_user_id: pseudoId,
-        ftm_language: lang,
-        profile_number: 4,
-        version_number: document.getElementById("version-info-id").innerHTML,
-        json_version_number: 1.2333,
-      });
+      setUserId(this.analytics,pseudoId);
+      this.userProperties.cr_user_id = event.cr_user_id;
+      this.userProperties.ftm_language = event.ftm_language;
+      this.userProperties.profile_number = event.profile_number;
+      this.userProperties.version_number = event.version_number;
+      this.userProperties.json_version_number =  new Date().getTime();
+      setUserProperties(this.analytics,this.userProperties);
+ 
       console.log(`Sending custom event ${eventName} with data:`, event);
+      
       logEvent(this.analytics, eventName, event);
     } catch (error) {
       console.error("Error while logging custom event:", error);
