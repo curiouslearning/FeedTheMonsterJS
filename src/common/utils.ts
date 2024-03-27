80bd5becd585457ac9d2a1d0de288f15eb664dcd
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2024-03-27 17:01:46 +0530

    Updating the build Folder and Removed all the build folder conflict's!

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -2,40 +2,42 @@ import { Debugger, lang } from "../../global-variables";
 import languageFontMapping from "../data/i18-font-mapping";
 
 export class Utils {
-    public static UrlSubstring: string = '/feedthemonster'
+  public static UrlSubstring: string = "/feedthemonster";
 
-    public static getConvertedDevProdURL(url: string): string {
-        return Debugger.DevelopmentLink
-            ? url.slice(
-                0,
-                url.indexOf(this.UrlSubstring) +
-                this.UrlSubstring.length
-            ) +
-            "dev" +
-            url.slice(
-                url.indexOf(this.UrlSubstring) +
-                this.UrlSubstring.length
-            )
-            : url;
-    }
+  public static getConvertedDevProdURL(url: string): string {
+    return Debugger.DevelopmentLink
+      ? url.slice(
+          0,
+          url.indexOf(this.UrlSubstring) + this.UrlSubstring.length
+        ) +
+          "dev" +
+          url.slice(url.indexOf(this.UrlSubstring) + this.UrlSubstring.length)
+      : url;
+  }
 
-    public static getLanguageSpecificFont(language: string): string {
-        const lowerCaseLanguage = language.toLowerCase();
-      
-        for (const key in languageFontMapping) {
-          if (key.toLowerCase() === lowerCaseLanguage) {
-            return languageFontMapping[key];
-          }
-        }
-        
-        console.log(`Font not found for language: ${language}`);
-        return 'NotoSans-Regular';
+  public static getLanguageSpecificFont(language: string): string {
+    const lowerCaseLanguage = language.toLowerCase();
+
+    for (const key in languageFontMapping) {
+      if (key.toLowerCase() === lowerCaseLanguage) {
+        return languageFontMapping[key];
       }
-      
-      public static getExcludedCoordinates(canvas: HTMLCanvasElement, exclusionPercentage: number): { excludeX: number, excludeY: number } {
-        const excludedAreaWidth = canvas.width * (exclusionPercentage / 100);
-        const excludedAreaHeight = canvas.height * (exclusionPercentage / 100);
-
-        return { excludeX: excludedAreaWidth, excludeY: excludedAreaHeight };
     }
+
+    console.log(`Font not found for language: ${language}`);
+    return "NotoSans-Regular";
+  }
+
+  public static getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  public static getExcludedCoordinates(
+    canvas: HTMLCanvasElement,
+    exclusionPercentage: number
+  ): { excludeX: number; excludeY: number } {
+    const excludedAreaWidth = canvas.width * (exclusionPercentage / 100);
+    const excludedAreaHeight = canvas.height * (exclusionPercentage / 100);
+
+    return { excludeX: excludedAreaWidth, excludeY: excludedAreaHeight };
+  }
 }
