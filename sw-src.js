0ccfd9fdd68af46bcfdd230f42a6473ef1a7bf57
amitsinghsutara <102941445+amitsinghsutara@users.noreply.github.com>
2024-05-30 15:47:31 +0530

    fm-175( fix delay of Swahili feedback audios!)
    I have updated the feedback audios of swahili audios!

--- sw-src.js
+++ sw-src.js
@@ -6,7 +6,7 @@ workbox.precaching.precacheAndRoute(self.__WB_MANIFEST, {
   exclude: [/^lang\//],
 });
 var number = 0;
-var version = 1.22;
+var version = 1.23;
 // self.addEventListener('activate', function(e) {
 //     console.log("activated");
 //
