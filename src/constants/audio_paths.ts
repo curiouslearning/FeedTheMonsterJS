8a771d766f7073c7919db98a48156795d677107a
Bernhard Cena <bernhardc@codev.com>
2024-07-22 21:02:31 +0800

    Optimization: Optimized the unsubscription of event listers in scene-handler, start-scene, levelend-scene and gameplay.

--- src/constants/audio_paths.ts
+++
@@ -1,8 +0,0 @@
-export const AUDIO_PATH_EATS = "./assets/audios/Eat.mp3";
-export const AUDIO_PATH_MONSTER_SPIT = "./assets/audios/MonsterSpit.mp3";
-export const AUDIO_PATH_MONSTER_DISSAPOINTED = "./assets/audios/Disapointed-05.mp3"
-export const AUDIO_PATH_POINTS_ADD = "assets/audios/PointsAdd.wav";
-export const AUDIO_PATH_CORRECT_STONE = "assets/audios/CorrectStoneFinal.mp3"
-export const AUDIO_PATH_CHEERING_FUNC = (randomNumber) => `./assets/audios/Cheering-0${randomNumber}.mp3`;
-export const AUDIO_PATH_BTN_CLICK = "./assets/audios/ButtonClick.mp3"
-export const AUDIO_PATH_ON_DRAG = "./assets/audios/onDrag.mp3"
