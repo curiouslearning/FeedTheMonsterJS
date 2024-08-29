dc6764909c86c8f751f63404c494a876f0b70848
Sheerwood John Caday <sheerwoodc@codev.com>
2024-08-29 16:10:58 +0800

    FM-246 - applied adjustments from PR feedback

--- src/scenes/levelend-scene.ts
+++ src/scenes/levelend-scene.ts
@@ -1,7 +1,6 @@
 import { loadImages, CLICK } from "@common";
-import { AudioPlayer } from "@components";
+import { AudioPlayer, Monster } from "@components";
 import { CloseButton, NextButton, RetryButton } from "@buttons";
-import { Monster } from "@components";
 import {
   BACKGROUND_ASSET_LIST,
   createBackground,
