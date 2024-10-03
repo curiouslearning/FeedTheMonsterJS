20874459d62d9ee0592343664e500692640f0c3e
Sheerwood John Caday <sheerwoodc@codev.com>
2024-10-03 18:12:06 +0800

    Fix: Remove Next Level Icon after completing the Last Level of FTM (FM-281)

--- src/scenes/levelend-scene.ts
+++ src/scenes/levelend-scene.ts
@@ -211,7 +211,7 @@ export class LevelEndScene {
   }
 
   handleMouseClick = (event) => {
-    const selfElement: HTMLElement =document.getElementById("canvas");
+    const selfElement:HTMLElement =document.getElementById("canvas");
     var rect = selfElement.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
