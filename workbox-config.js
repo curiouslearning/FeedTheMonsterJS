25448932260e3d02114e4622d911d4cf72556b51
Miguel Calo <miguelc@codev.com>
2024-10-25 16:16:00 +0800

    =chore: applied comment

--- workbox-config.js
+++ workbox-config.js
@@ -1,20 +1,5 @@
-// <<<<<<< HEAD
-// const path = require('path');
-// =======
-// module.exports = {
-//   globDirectory: "build/",
-//   globPatterns: [
-//     "**/*.{wav,mp3,WAV,png,jpg,webp,js,gif,css,html}",
-    
-//     "./manifest.json"
-//   ],
-//   maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
-//   swDest: "build/sw.js",
-//   swSrc: "src/sw-src.js",
-//   globIgnores: [
-//     "lang/**/*.{wav,mp3,WAV,png,jpg,webp}",
-// >>>>>>> pre-develop
 const path = require('path');
+
 module.exports = {
   globDirectory: "build/",
   maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
