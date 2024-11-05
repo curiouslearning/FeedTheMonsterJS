911b86708ec0aebffcad52bf4bdd37561d52192e
Sheerwood John Caday <sheerwoodc@codev.com>
2024-11-05 15:42:57 +0800

    feat: revert some code (FM-296)

--- src/components/popups/sure-popup.ts
+++ src/components/popups/sure-popup.ts
@@ -1,7 +1,7 @@
-import {CLICK, loadImages} from '@common';
-import {AudioPlayer} from '@components';
-import {YesButton, NoButton} from '@buttons';
-import {POPUP_BG_IMG} from '@constants';
+import { CLICK, loadImages } from "@common";
+import { AudioPlayer } from "@components";
+import { YesButton, NoButton } from "@buttons";
+import { POPUP_BG_IMG } from "@constants";
 
 export default class AreYouSurePopUp {
   public canvas: HTMLCanvasElement;
@@ -16,11 +16,11 @@ export default class AreYouSurePopUp {
     this.canvas = canvas;
     this.yesCallback = yesCallback;
     this.noCallback = noCallback;
-    this.context = this.canvas.getContext('2d');
+    this.context = this.canvas.getContext("2d");
     this.audioPlayer = new AudioPlayer();
 
-    loadImages({pop_up_image: POPUP_BG_IMG}, images => {
-      this.pop_up_image = images['pop_up_image'];
+    loadImages({ pop_up_image: POPUP_BG_IMG }, (images) => {
+      this.pop_up_image = images["pop_up_image"];
       this.imagesLoaded = true;
     });
   }
@@ -53,21 +53,21 @@ export default class AreYouSurePopUp {
 
   draw() {
     if (this.imagesLoaded) {
-      this.context.fillStyle = 'rgba(0,0,0,0.5)';
+      this.context.fillStyle = "rgba(0,0,0,0.5)";
       this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
       this.context.drawImage(
         this.pop_up_image,
         this.canvas.width * 0.1,
         this.canvas.height * 0.2,
         this.canvas.width * 0.8,
-        this.canvas.width * 0.8,
+        this.canvas.width * 0.8
       );
 
       const textY = this.canvas.height * 0.2 + 80;
-      this.context.fillStyle = 'white';
-      this.context.font = '24px Arial';
+      this.context.fillStyle = "white";
+      this.context.font = "24px Arial";
       this.context.fillText(
-        'Are you sure?',
+        "Are you sure?",
         this.canvas.width / 2,
         this.canvas.height / 2.8,
       );
