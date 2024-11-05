5ed5601157d4be000528758aa7705390205c86ea
Sheerwood John Caday <sheerwoodc@codev.com>
2024-11-05 19:09:57 +0800

    feat: update pause-button component (FM-296)

--- src/components/buttons/pause-button/pause-button.ts
+++ src/components/buttons/pause-button/pause-button.ts
@@ -2,13 +2,18 @@ import {PAUSE_BTN_IMG} from '@constants';
 import {BaseButtonComponent} from '@components/buttons/base-button-component/base-button-component';
 
 export default class PauseButton extends BaseButtonComponent {
-  constructor() {
+  constructor(
+    customId = 'pause-button',
+    customClassName = 'pause-button-image',
+    customImageAlt = 'Pause Icon',
+    customTargetId = 'game-control',
+  ) {
     super({
-      id: 'pause-button',
-      className: 'pause-button-image',
+      id: customId,
+      className: customClassName,
       imageSrc: PAUSE_BTN_IMG,
-      imageAlt: 'Pause Icon',
-      targetId: 'game-control',
+      imageAlt: customImageAlt,
+      targetId: customTargetId,
     });
   }
 }
