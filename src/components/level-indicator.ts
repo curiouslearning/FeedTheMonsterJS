1563137c5a0e12e3ac34dcbed632f08cea2d1b1f
Nikhil77sutara <nikhilchoudhary@sutara.org>
2024-11-13 13:36:59 +0530

    fix: release notes

--- src/components/level-indicator.ts
+++ src/components/level-indicator.ts
@@ -2,6 +2,7 @@ import { loadImages } from "@common";
 import { BAR_EMPTY, BAR_FULL, LEVEL_INDICATOR } from "@constants";
 import { EventManager } from "@events";
 
+//testing the release notes
 
 export class LevelIndicators extends EventManager{
     public context: CanvasRenderingContext2D;
