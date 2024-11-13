597ec54090a4088c205a3d4096f525af6eeeae99
bernhardccodev <bernhardc@codev.com>
2024-11-13 15:07:59 +0800

    Feat: Refactor the Level Indicator from Canvas to HTML (FM-311) (#1419)
    * feat: Converted Level indicator from Canvas to HTML approach (FM-311).
    
    * feat: Code clean up and added unit testing (FM-311).
    
    * feat: Calls destroy method directly (FM-311)
    
    * feat: Addressed review comments (FM-311).

---
+++ src/components/level-field/level-field-component.ts
@@ -0,0 +1,55 @@
+import {
+  BaseHtmlOptions,
+  BaseHTML
+} from '../baseHTML/base-html';
+import {
+  BAR_EMPTY,
+  BAR_FULL,
+  LEVEL_INDICATOR
+} from "@constants";
+import './level-field-component.scss';
+
+export const DEFAULT_SELECTORS = {
+  root: '.game-scene', //Class name of game scene div.
+  bars: '.bar-level'
+};
+
+
+export const LEVEL_FIELD_LAYOUT = (id: string) => {
+  return (`
+    <div id="${id}" class="level_content-wrapper">
+      <div class="level-field">
+        <img class="level-background" src="${LEVEL_INDICATOR}"/>
+        <div class="bar-level-wrapper">
+           ${Array(5).fill(`<img class="bar-level" src="${BAR_EMPTY}" alt="bar" />`).join('')}
+        </div>
+      </div>
+    </div>
+  `);
+}
+
+export default class LevelFieldComponent extends BaseHTML {
+
+  constructor(
+    id: string = 'level-field',
+    options: BaseHtmlOptions = { selectors: DEFAULT_SELECTORS }
+  ){
+    super(
+      options,
+      id,
+      LEVEL_FIELD_LAYOUT
+    );
+  }
+
+  updateLevel(index: number):void {
+    const barIndex = index - 1;
+    if (barIndex < 0) return;
+
+    const { bars } = this.options.selectors;
+    const barsEl = this.getElements(bars);
+
+    if (barsEl.length && barsEl[barIndex]) {
+      barsEl[barIndex].src = BAR_FULL;
+    }
+  }
+}
