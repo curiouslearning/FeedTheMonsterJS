c906fb59abeef10776a12196240e45a3f1f3335d
Bernhard Cena <bernhardc@codev.com>
2024-11-26 22:06:20 +0800

    fix: Fix the left stones from adjusting their original X position.

--- src/gameStateService/data-access-objects.ts
+++ src/gameStateService/data-access-objects.ts
@@ -72,30 +72,34 @@ export const createStonePositionsDAO = ({
 		*/
 		return deviceWidth > widthVal ? smallerVal : defaultVal;
 	}
+
   //Coordinates with comments are the ones near to the monster.
 	const staticCoordinateFactors = [
-		[5, 1.9],
-		[setCoordinateFactor(2, 1.3), 1.07], //This stone is located right below the monster.
-		[[setCoordinateFactor(2.8, 2.5), 2], 1.2], //Lower right 1.
-		[setCoordinateFactor(4.3, 4.5), 1.28], //lower left side 2.
-		[7, 1.5],
-		[[2.3, 2.1],  1.9],
-		[[setCoordinateFactor(3, 2.4), 2.1], 1.42],  //Lower right 2, above lower right 1.
-		[6.4, 1.1]
+		[5, 1.9], //Left stone 1 - upper
+		[7, 1.5], //Left stone 2
+		[setCoordinateFactor(4.3, 4.5), 1.28], //Left stone 3
+		[6.4, 1.1], //Left stone 4 - very bottom
+		[setCoordinateFactor(2, 1.3), 1.07], //Middle stone that is located right below the monster.
+		[[2.3, 2.1], 1.9], //Right stone 1 - upper
+		[[setCoordinateFactor(2.8, 2.5), 2], 1.2], //Right stone 2
+		[[setCoordinateFactor(3, 2.4), 2.1], 1.42],  //Right stone 3
 	];
 
 	const randomizedStonePositions = staticCoordinateFactors.map(
-	(coordinatesFactors: [number[] | number, number]) => {
+	(coordinatesFactors: [number[] | number, number], index) => {
 		const factorX = coordinatesFactors[0];
 		const factorY = coordinatesFactors[1];
 		let coordinateX = Array.isArray(factorX)
 			? ((widthVal / factorX[0]) + (widthVal / factorX[1]))
 			: (widthVal / factorX);
 		let coordinateY = heightVal / factorY;
+		const offsetXAdjustment = index < 4 ? 25 : 0; //Only use +25 on stones on the left side.
+		const posX = coordinateX - offsetCoordinateValue;
+		const posY = coordinateY - offsetCoordinateValue;
 
 		return [
-				coordinateX - offsetCoordinateValue,
-				coordinateY - offsetCoordinateValue,
+			posX + offsetXAdjustment,
+			posY,
 		]
 	}
 	).sort(() => Math.random() - 0.5);
