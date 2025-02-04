032eae055c2d1e6b8c1255d60ddb0e8329182f9b
ashwinnair-chimple <ashwin.nair@sutara.org>
2025-02-04 15:03:20 +0530

    Revert "test:FM-370 Pixelation investigation: 2 experiments within a small and a big image on DOM and canvas (#1526)"
    This reverts commit 03b2b4fd99a06944028703c6989e746db9f5fcc7.

--- src/scenes/start-scene/start-scene.ts
+++ src/scenes/start-scene/start-scene.ts
@@ -35,8 +35,6 @@ export class StartScene {
   public switchSceneToLevelSelection: Function;
   audioPlayer: AudioPlayer;
   private toggleBtn: HTMLElement;
-  private toggleSVG: HTMLElement;
-  private togglecanvasBtn: HTMLElement;
   private pwa_install_status: Event;
   private titleTextElement: HTMLElement | null;
   public riveMonster: RiveMonsterComponent;
@@ -52,11 +50,8 @@ export class StartScene {
     switchSceneToLevelSelection: Function
   ) {
     this.data = data;
-    this.canavsElement = document.getElementById("canvas") as HTMLCanvasElement;
     this.riveMonsterElement = document.getElementById("rivecanvas") as HTMLCanvasElement;
     this.toggleBtn = document.getElementById("toggle-btn") as HTMLElement;
-    this.toggleSVG = document.getElementById("toggleSVG") as HTMLElement;
-    this.togglecanvasBtn = document.getElementById("toggleCanvas") as HTMLElement;
     this.loadingElement = document.getElementById("loading-screen") as HTMLElement;
     this.riveMonster = new RiveMonsterComponent({
       canvas: this.riveMonsterElement,
@@ -71,18 +66,18 @@ export class StartScene {
         this.hideInitialLoading();
         // this.riveMonster.play(RiveMonsterComponent.Animations.MOUTHOPEN); // Start with the "Idle" animation
         // Trigger a "Happy" animation
-        // Set initial state inputs
-        //  this.riveMonster.setInput(RiveMonsterComponent.Animations.IDLE,true);
+       // Set initial state inputs
+      //  this.riveMonster.setInput(RiveMonsterComponent.Animations.IDLE,true);
 
-        // Listen for state changes
-        //  this.riveMonster.onStateChange((stateName) => {
-        //      console.log('New State:', stateName);
-        //  });
+       // Listen for state changes
+      //  this.riveMonster.onStateChange((stateName) => {
+      //      console.log('New State:', stateName);
+      //  });
 
-        // Example: Trigger "Sad" state after 2 seconds
-        setTimeout(() => {
+       // Example: Trigger "Sad" state after 2 seconds
+       setTimeout(() => {
           //  this.riveMonster.setInput(RiveMonsterComponent.Animations.STOMP,true);
-        }, 2000);
+       }, 2000);
       }
     });
 
@@ -107,15 +102,14 @@ export class StartScene {
         }
       },
       'start-scene-click-area',
-      (id) => (`<div id="${id}"></div>`), true
+      (id) => (`<div id="${id}"></div>`),
+      true
     );
     this.switchSceneToLevelSelection = switchSceneToLevelSelection;
     this.audioPlayer = new AudioPlayer();
     this.pwa_status = localStorage.getItem(PWAInstallStatus);
     this.handler = document.getElementById('start-scene-click-area') as HTMLBodyElement;
     this.devToggle();
-    this.toggleSvg();
-    this.toggleCanvas();
     this.createPlayButton();
     window.addEventListener("beforeinstallprompt", this.handlerInstallPrompt);
     this.setupBg();
@@ -164,49 +158,6 @@ export class StartScene {
     );
   };
 
-  toggleCanvas= () =>{
-    this.togglecanvasBtn.addEventListener("click", ()=>{
-      const element = document.getElementById('svgcanvas');
-      if (element) {
-        // Toggle the display style between 'none' and 'block'
-        if (element.style.display === "none" || element.style.display === "") {
-          element.style.display = "block"; // Show the element
-          this.riveMonsterElement.style.display = "none";
-        } else {
-          element.style.display = "none"; // Hide the element
-          this.riveMonsterElement.style.display = "block";
-        }
-      } else {
-        console.warn(`Element with ID "${element}" not found.`);
-      }
-    })
-  }
-
-  toggleSvg = () => {
-    this.toggleSVG.addEventListener("click", () => {
-      const element = document.getElementById('svg-img');
-      const elementtwo = document.getElementById('cloudpng');
-      const elementthree = document.getElementById('pinstar');
-      if (element) {
-        // Toggle the display style between 'none' and 'block'
-        if (element.style.display === "none" || element.style.display === "") {
-          element.style.display = "block"; // Show the element
-          this.riveMonsterElement.style.display = "none";
-          elementtwo.style.display = "block"
-          elementthree.style.display = "block"
-        } else {
-          element.style.display = "none"; // Hide the element
-          this.riveMonsterElement.style.display = "block";
-          elementtwo.style.display = "none"
-          elementthree.style.display = "none"
-        }
-      } else {
-        console.warn(`Element with ID "${element}" not found.`);
-      }
-
-    })
-  }
-
   generateGameTitle = () => {
     if (this.titleTextElement) {
       this.titleTextElement.textContent = this.data.title;
@@ -232,7 +183,7 @@ export class StartScene {
     document.addEventListener("selectstart", function (e) {
       e.preventDefault();
     });
-    this.handler.addEventListener("click", this.handleMouseClick, false); 
+    //this.handler.addEventListener("click", this.handleMouseClick, false); //Doesn't work adding on riveCanvas and doesn't work anymore due to riveCanvas using full width and height.
   }
 
   handleMouseClick = (event) => {
