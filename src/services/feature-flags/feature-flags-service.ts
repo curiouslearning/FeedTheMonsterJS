cb3036255d6dd8caf8408bf6f3e75c1cd30ffdd1
Miguel Calo <miguelc@codev.com>
2025-02-13 19:27:52 +0800

    chore: use @curiouslearning/features module isntead

--- src/services/feature-flags/feature-flags-service.ts
+++ src/services/feature-flags/feature-flags-service.ts
@@ -1,43 +1,5 @@
 
 import { pseudoId } from '@common';
-import { StatsigClient } from '@statsig/js-client';
-import { LocalStorageCache } from '../cache/local-storage';
-import { FEATURE_QUICK_START } from './features';
-
-// TODO: move this to env variable
-const STATSIG_CLIENT_KEY = 'client-SSmY5k5Cs39G7II74NdWqPfv5hQzrFiUqCc3C1IU9na';
-
-export class FeatureFlagsService {
-  private storage = new LocalStorageCache('FtmFeatureFlag');
-  private statsigClient: StatsigClient;
-
-  constructor(public metaData) {
-    this.statsigClient = new StatsigClient(STATSIG_CLIENT_KEY, { userID: metaData?.userId || null });
-  }
-
-  async initialize() {
-    try {
-      await this.statsigClient.initializeAsync();
-      this.loadAndStore();
-    } catch (e) {
-      // do nothing, or catch errors when in PWA context. Here, we rely on whatever's stored in localstorage.
-    }
-  }
-
-  /**
-   * TODO: create localstorage cache to store flags
-   */
-  loadAndStore() {
-    [
-      FEATURE_QUICK_START
-    ].forEach((feature) => {
-      this.storage.set(feature, this.statsigClient.checkGate(feature))
-    });
-  }
-
-  isEnabled(key: string) {
-    return this.storage.get(key);
-  }
-}
+import { FeatureFlagsService } from '@curiouslearning/features';
 
 export const featureFlagService = new FeatureFlagsService({ userId: pseudoId || null });
