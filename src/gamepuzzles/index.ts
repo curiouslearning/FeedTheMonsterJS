0acd616a1fb49e08006da907e544269e3f8c364f
Sheerwood John Caday <sheerwoodc@codev.com>
2025-04-16 22:01:01 +0800

    feat: Implement FeedbackAudioHandler for audio feedback (FM-468)

--- src/gamepuzzles/index.ts
+++ src/gamepuzzles/index.ts
@@ -1,4 +1,9 @@
 import WordPuzzleLogic from './wordPuzzleLogic';
+import FeedbackAudioHandler, { FeedbackType } from './feedbackAudioHandler/feedbackAudioHandler';
 //import here for single letter puzzle or other game puzzle logic.
 
-export { WordPuzzleLogic };
+export { 
+  WordPuzzleLogic,
+  FeedbackAudioHandler,
+  FeedbackType
+};
