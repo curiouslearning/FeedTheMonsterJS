89cc5cc4cc205f341a663a4f738287375f54e1f1
Sheerwood John Caday <sheerwoodc@codev.com>
2025-04-16 19:53:22 +0800

    fix: the prompt text highlighting when it has more than 1 or more characters in foilstones

--- src/scenes/gameplay-scene/gameplay-scene.ts
+++ src/scenes/gameplay-scene/gameplay-scene.ts
@@ -756,10 +756,12 @@ export class GameplayScene {
       this.timerTicking.startTimer();
       // // Trigger animations via fire
       // this.triggerMonsterAnimation('isHappy',3000)
+      const { droppedHistory } = this.wordPuzzleLogic.getValues();
+      const droppedStonesCount = Object.keys(droppedHistory).length;
       this.promptText.droppedStoneIndex(
         lang == "arabic"
           ? this.stonesCount
-          : droppedLetters.length
+          : droppedStonesCount
       );
       this.stonesCount++;
     } else {
