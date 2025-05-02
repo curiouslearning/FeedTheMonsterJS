765cf9215f96f4aca5cc55ce207aa3cbfc408109
ashwinnair-chimple <ashwin.nair@sutara.org>
2025-05-02 18:16:03 +0530

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -86,7 +86,6 @@ var config = {
         { from: "./public/index.css", to: "./" },
         { from: "./public/assets", to: "./assets" },
         { from: "./lang", to: "./lang" },
-        { from: './public/assets/rive.wasm', to: './assets/rive.wasm' },
       ],
     }),
 
