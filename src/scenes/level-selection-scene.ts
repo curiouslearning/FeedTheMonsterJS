00cdc31fcff05cb84a042030cb594ed1ce3bd0ba
Bernhard Cena <bernhardc@codev.com>
2025-11-10 12:03:25 +0800

    debug: Added logs to where the bugs occurs in prod (FM-748 and FM-749)

--- src/scenes/level-selection-scene.ts
+++ src/scenes/level-selection-scene.ts
@@ -135,6 +135,7 @@ export class LevelSelectionScreen {
 
   private async createLevelButtons() {
     const images = await loadLevelImages();
+    console.log('Level Selection Assets: ', { images })
     const poss = getdefaultCloudBtnsPos(this.canvas);
     const totalLevels = this.data.levels.length;
     const buttonsPerPage = 10;
