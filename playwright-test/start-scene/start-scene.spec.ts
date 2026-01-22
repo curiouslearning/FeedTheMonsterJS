33480df7d83b1681b7cfa1f2f7a0b6ef7c116566
ashwinnair-chimple <ashwin.nair@sutara.org>
2026-01-22 11:06:55 +0530

    chore: playwright changes

--- playwright-test/start-scene/start-scene.spec.ts
+++ playwright-test/start-scene/start-scene.spec.ts
@@ -1,45 +1,45 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-// test.describe('StartScene', () => {
-//   test.beforeEach(async ({ page }) => {
-//     await page.goto('https://feedthemonsterdev.curiouscontent.org/');
-//   });
+// // test.describe('StartScene', () => {
+// //   test.beforeEach(async ({ page }) => {
+// //     await page.goto('https://feedthemonsterdev.curiouscontent.org/');
+// //   });
 
-//   test('should hide loading screen after init', async ({ page }) => {
-//     const loading = page.locator('#loading-screen');
-//     await expect(loading).toBeVisible();
-//     await expect(loading).toBeHidden();
-//   });
+// //   test('should hide loading screen after init', async ({ page }) => {
+// //     const loading = page.locator('#loading-screen');
+// //     await expect(loading).toBeVisible();
+// //     await expect(loading).toBeHidden();
+// //   });
 
-//   test('should redirect to Level Select on background click', async ({ page }) => {
-//     const clickArea = page.locator('#start-scene-click-area');
-//     await expect(clickArea).toBeVisible();
-//     await clickArea.click();
+// //   test('should redirect to Level Select on background click', async ({ page }) => {
+// //     const clickArea = page.locator('#start-scene-click-area');
+// //     await expect(clickArea).toBeVisible();
+// //     await clickArea.click();
 
-//     await page.waitForFunction(() =>
-//       document.body.innerText.includes('Level Select')
-//     );
-//   });
-// });
+// //     await page.waitForFunction(() =>
+// //       document.body.innerText.includes('Level Select')
+// //     );
+// //   });
+// // });
 
 
-test('Title page', async ({page})=>{
+// test('Title page', async ({page})=>{
 
-await page.goto('https://feedthemonsterdev.curiouscontent.org/');
+// await page.goto('https://feedthemonsterdev.curiouscontent.org/');
 
-const pageTitle = await page.title();
-console.log('Page Title is:' , pageTitle);
+// const pageTitle = await page.title();
+// console.log('Page Title is:' , pageTitle);
 
-await expect(page).toHaveTitle('Feed The Monster');
+// await expect(page).toHaveTitle('Feed The Monster');
 
-await expect(page).toHaveURL('https://feedthemonsterdev.curiouscontent.org/');
+// await expect(page).toHaveURL('https://feedthemonsterdev.curiouscontent.org/');
 
-//await page.click('id=title-and-play-button')
+// //await page.click('id=title-and-play-button')
 
-await page.waitForSelector('#play-button', { timeout: 60000 });
+// await page.waitForSelector('#play-button', { timeout: 60000 });
 
-await page.click('#play-button');
-await page.goto('https://feedthemonsterdev.curiouscontent.org/');
-//await page.close();
+// await page.click('#play-button');
+// await page.goto('https://feedthemonsterdev.curiouscontent.org/');
+// //await page.close();
 
-})
+// })
